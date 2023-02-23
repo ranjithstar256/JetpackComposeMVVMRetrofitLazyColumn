f8042053e316762ef14382958efef39cae975b97
Ranjith <ranjithstar256@gmail.com>
2023-02-23 17:40:40 +0530

    added second screen which will display news in a different activity. added DisplayNews.kt class. you guys work in improving UI Functionality completed.

--- app/src/main/java/com/example/jetpackcomposemvvmretrofitandrecyclerview/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposemvvmretrofitandrecyclerview/MainActivity.kt
@@ -1,27 +1,40 @@
 package com.example.jetpackcomposemvvmretrofitandrecyclerview
 
+import android.content.Context
+import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.animation.expandHorizontally
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.text.HtmlCompat
+import androidx.core.view.ContentInfoCompat.Flags
 import coil.compose.rememberImagePainter
 import coil.size.Scale
 import coil.transform.CircleCropTransformation
@@ -30,30 +43,34 @@ import com.example.jetpackcomposemvvmretrofitandrecyclerview.ui.theme.JetpackCom
 
 class MainActivity : ComponentActivity() {
     val mainViewModel by viewModels<MainViewModel>()
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeMVVMRetrofitAndRecyclerviewTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    MovieList(movieList = mainViewModel.movieListResponse)
-                    mainViewModel.getMovieList()
+                    Column() {
+
+
+                        Text(text = "Latest NEWS", fontSize = 32.sp, modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.Center)
+
+                        MovieList(applicationContext, movieList = mainViewModel.movieListResponse)
+                        mainViewModel.getMovieList()
+                    }
                 }
             }
         }
     }
 }
 
-
 @Composable
-fun MovieList(movieList: List<Articles>) {
+fun MovieList(context: Context,movieList: List<Articles>) {
     var selectedIndex by remember { mutableStateOf(-1) }
     LazyColumn {
 
-        itemsIndexed(items = movieList) { index, item ->
-            MovieItem(movie = item, index, selectedIndex) { i ->
+        itemsIndexed(items = movieList) {
+                index, item ->
+            MovieItem(context,movie = item, index, selectedIndex) { i ->
                 selectedIndex = i
             }
         }
@@ -61,9 +78,8 @@ fun MovieList(movieList: List<Articles>) {
 
 }
 
-@Preview(showBackground = true)
 @Composable
-fun MovieItem() {
+fun MovieItem(context: Context) {
     val movie = Articles(
         "Coco",
         "",
@@ -71,23 +87,29 @@ fun MovieItem() {
     )
 
     
-    MovieItem(movie = movie, 0, 0) { i ->
-
+    MovieItem(context,movie = movie, 0, 0) { i ->
+        Log.i("wertytest123abc", "MovieItem: "
+                +i)
     }
 }
 
-
 @Composable
-fun MovieItem(movie: Articles, index: Int, selectedIndex: Int, onClick: (Int) -> Unit) {
+fun MovieItem(context: Context,movie: Articles, index: Int, selectedIndex: Int,
+              onClick: (Int) -> Unit)
+{
 
-    val backgroundColor =
-        if (index == selectedIndex) MaterialTheme.colors.primary else MaterialTheme.colors.background
+    val backgroundColor = if (index == selectedIndex) MaterialTheme.colors.primary else MaterialTheme.colors.background
+
     Card(
         modifier = Modifier
             .padding(8.dp, 4.dp)
-            .fillMaxWidth()
+            .fillMaxSize()
+            .selectable(true, true, null,
+                onClick = {
+                    Log.i("test123abc", "MovieItem: $index/n$selectedIndex")
+                })
             .clickable { onClick(index) }
-            .height(110.dp), shape = RoundedCornerShape(8.dp), elevation = 4.dp
+            .height(180.dp), shape = RoundedCornerShape(8.dp), elevation = 4.dp
     ) {
         Surface(color = backgroundColor) {
 
@@ -95,23 +117,22 @@ fun MovieItem(movie: Articles, index: Int, selectedIndex: Int, onClick: (Int) ->
                 Modifier
                     .padding(4.dp)
                     .fillMaxSize()
-            ) {
 
+            )
+             {
                 Image(
                     painter = rememberImagePainter(
                         data = movie.urlToImage,
-
                         builder = {
                             scale(Scale.FILL)
                             placeholder(R.drawable.placeholder)
                             transformations(CircleCropTransformation())
-
                         }
                     ),
                     contentDescription = movie.description,
                     modifier = Modifier
                         .fillMaxHeight()
-                        .weight(0.2f)
+                        .weight(0.3f)
                 )
 
 
@@ -121,41 +142,148 @@ fun MovieItem(movie: Articles, index: Int, selectedIndex: Int, onClick: (Int) ->
                         .padding(4.dp)
                         .fillMaxHeight()
                         .weight(0.8f)
+                        .background(Color.Yellow)
+                        .padding(20.dp)
+                        .selectable(true, true, null,
+                            onClick = {
+                                Log.i("test123abc", "MovieItem: $index/n${movie.description}")
+                                context.startActivity(
+                                    Intent(context, DisplayNews::class.java)
+                                        .setFlags(FLAG_ACTIVITY_NEW_TASK)
+                                        .putExtra("desk", movie.description.toString())
+                                        .putExtra("urlToImage", movie.urlToImage)
+                                        .putExtra("title", movie.title)
+                                )
+                            })
                 ) {
+
                     Text(
                         text = movie.title.toString(),
                         style = MaterialTheme.typography.subtitle1,
                         fontWeight = FontWeight.Bold
                     )
-                    Text(
-                        text = movie.description.toString(),
-                        style = MaterialTheme.typography.caption,
-                        modifier = Modifier
-                            .background(
-                                Color.LightGray
-                            )
-                            .padding(4.dp)
-                    )
-                    Text(
-                        text = movie.description.toString(),
-                        style = MaterialTheme.typography.body1,
-                        maxLines = 2,
-                        overflow = TextOverflow.Ellipsis
-                    )
 
+                    HtmlText(html = movie.description.toString())
                 }
             }
         }
     }
-
+    @Composable
+    fun HtmlText(html: String, modifier: Modifier = Modifier) {
+        AndroidView(
+            modifier = modifier
+                .fillMaxSize()
+                .size(33.dp),
+            factory = { context -> TextView(context) },
+            update = { it.text = HtmlCompat.fromHtml(html, HtmlCompat.FROM_HTML_MODE_COMPACT) }
+        )
+    }
 }
 
 
 
 
 
-
 /*
+@Composable
+fun Content() {
+    val context = LocalContext.current
+    var showCustomDialog by remember { mutableStateOf(false) }
+
+    Column(
+        Modifier.fillMaxSize(),
+        Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { showCustomDialog = !showCustomDialog }, Modifier.wrapContentSize()) {
+            Text(text = "Show Alert Dialog")
+        }
+    }
+
+    if (showCustomDialog) {
+        CustomAlertDialog({
+            showCustomDialog = !showCustomDialog
+        }, {
+            val activity = (context as? Activity)
+            activity?.finish()
+        })
+    }
+}
+
+@Composable
+fun CustomAlertDialog(onDismiss: () -> Unit, onExit: () -> Unit) {
+
+    Dialog(
+        onDismissRequest = { onDismiss() }, properties = DialogProperties(
+            dismissOnBackPress = false, dismissOnClickOutside = false
+        )
+    ) {
+        Card(
+            //shape = MaterialTheme.shapes.medium,
+            shape = RoundedCornerShape(10.dp),
+            // modifier = modifier.size(280.dp, 240.dp)
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            elevation = 8.dp
+        ) {
+            Column(
+                Modifier
+                    .fillMaxWidth()
+                    .background(Color.White)
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(100.dp)
+                        .background(Color.Red.copy(alpha = 0.8F)),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Image(
+                        painter = painterResource(id = android.R.drawable.star_big_on),
+                        contentDescription = "Exit app",
+                        modifier = Modifier.fillMaxSize(),
+                        contentScale = ContentScale.FillWidth
+                    )
+                }
+
+                Text(
+                    text = "Lorem Ipsum is simply dummy text",
+                    modifier = Modifier.padding(8.dp), fontSize = 20.sp
+                )
+
+                Text(
+                    text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard",
+                    modifier = Modifier.padding(8.dp)
+                )
+
+                Row(Modifier.padding(top = 10.dp)) {
+                    OutlinedButton(
+                        onClick = { onDismiss() },
+                        Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                            .weight(1F)
+                    ) {
+                        Text(text = "Cancel")
+                    }
+
+                    Button(
+                        onClick = { onExit() },
+                        Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                            .weight(1F)
+                    ) {
+                        Text(text = "Exit")
+                    }
+                }
+            }
+        }
+    }
+}
+
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
